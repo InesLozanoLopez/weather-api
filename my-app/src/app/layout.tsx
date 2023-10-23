af0b47f79dda55c6cfd79cb925a0882621857e5a
Ines Lozano <ineslozano@MacBook-Pro-de-Ines.local>
2023-10-23 19:54:30 +0100

    feat: page layout

---
+++ my-app/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import '@/globals.css';
+
+export const metadata: Metadata = {
+    title: 'Weather app',
+    description: 'weather app to know the weather on a specific location'
+};
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+})
+{    return(
+        <html lang='en'>
+        <head>
+        <link rel="icon" href="/appIcon.png"/>
+        </head>
+        <body>{children}</body>
+        </html>
+    )
+}
